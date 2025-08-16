eb7a90851cff0e45e1a1834e22791b325b33dd66
Pari55051 <xpari5505@gmail.com>
2025-08-16 09:02:13 +0530

    hover tooltip done + added base animation on fish

---
+++ script.js
@@ -0,0 +1,67 @@
+const fishes = document.querySelectorAll('.fish')
+let fish = [...fishes]
+
+// console.log([...fish])
+fish.forEach(item => {
+    item.addEventListener('mouseover', handleMouseOver)
+    item.addEventListener('mouseleave', handleMouseLeave)
+    item.addEventListener('mousemove', handleMouseMove)
+})
+
+function handlePosition(e) {
+    const ID = e.target.getAttribute('data-hoverDiv-id')
+    // console.log(ID)
+    const wrapper = document.getElementById(ID)
+
+    let top = ""
+
+    if (!(e.target.getBoundingClientRect().top + wrapper.offsetHeight > innerHeight)) {
+        top = `${e.clientY + e.target.offsetHeight}px`
+    } else {
+        top = `${e.clientY - (wrapper.offsetHeight + e.target.offsetHeight)}px`
+    }
+
+    return `position: fixed; left: ${e.clientX - wrapper.offsetWidth / 2}px; top:${top};`
+}
+
+
+function handleMouseOver (e) {
+    const hoverContent = e.target.getAttribute("data-info")
+    // console.log(hoverContent)
+
+    const ID = Math.random().toString(36).substring(2,9)
+    // console.log(ID)
+
+    const wrapper = document.createElement('div')
+    e.target.setAttribute('data-hoverDiv-id', ID)
+    wrapper.setAttribute('data-hoverDiv-wrapper', '')
+    wrapper.setAttribute("id", ID)
+
+    // console.log(wrapper)
+
+    wrapper.setAttribute('style', 'opacity: 1; transform: sclae(0.8);')
+
+    wrapper.innerHTML = hoverContent
+    document.body.append(wrapper)
+
+    wrapper.setAttribute('style', handlePosition(e))
+}
+
+
+function handleMouseLeave (e) {
+    const ID = e.target.getAttribute('data-hoverDiv-id')
+
+    document.getElementById(ID).style.opacity = 0
+    document.getElementById(ID).style.transform = "scale(0.8)"
+
+    setTimeout(() => {
+        document.getElementById(ID).remove()
+    }, 150)
+}
+
+
+function handleMouseMove (e) {
+    const ID = e.target.getAttribute('data-hoverDiv-id')
+    const wrapper = document.getElementById(ID)
+    wrapper.setAttribute("style", handlePosition(e))
+}
